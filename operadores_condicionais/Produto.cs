e39b9577ae556980bf3d1199a504d2a24bb9279f
Silas Paixão Santos <46866601+silasvianet@users.noreply.github.com>
2021-06-18 14:33:17 -0300

    Initial commit

---
+++ operadores_condicionais/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace operadores_condicionais
+{
+    public class Produto
+    {
+        public int    Codigo    { get; set; }
+        public string Descricao { get; set; }
+        public double Valor     { get; set; }
+    }
+}
---
+++ operadores_condicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace operadores_condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> oLSt = new List<int>();
+
+            int? x = oLSt?[0];
+
+            int? nome = 500;
+
+            nome = nome ?? 100;
+
+            Console.WriteLine(nome);
+            Console.WriteLine(GetProduto().Descricao);
+        }
+
+        public static Produto GetProduto()
+        {
+            var x = new Produto
+            {
+                Codigo = 1,
+                Descricao = "cabo",
+                Valor = 10.3
+            };
+
+            return x;
+        }
+
+    }
+}
